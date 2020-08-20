b50f88fc333f72522905b54c349df39a40174fc0
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-08-19 21:38:13 -0300

    Integrate list, add and remove with API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const resource = "/repositories";
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get(resource).then(({ data }) => setRepositories(data));
+  }, []);
+
+  function handleAddRepository() {
+    const repository = {
+      title: `It's me, ReactJS! - ${Date.now()}`,
+      url: "https://reactjs.org",
+      techs: ["JavaScript", "TypeScript"]
+    };
+    api.post(resource, repository)
+      .then(({ data }) => setRepositories([...repositories, data]));
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  function handleRemoveRepository(id) {
+    api.delete(`${resource}/${id}`)
+      .then(() => {
+        setRepositories(repositories.filter(repository => repository.id !== id));
+      });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
